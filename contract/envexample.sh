185b0a6c8743cfdc8cd0160cdd89ee8cc7305538
abdulsamed <abdalsamed71@gmail.com>
2025-06-04 22:23:17 +0000

    Update .gitignore to include .env, fix chainlink URL in .gitmodules, add envexample.sh for environment variables, and update openzeppelin-contracts submodule

---
+++ contract/envexample.sh
@@ -0,0 +1,9 @@
+# This file is used to set environment variables for the contract
+Sepolia_ETHERSCAN_API_KEY=
+MAINNET_RPC_URL=https://mainn
+SEPOLIA_RPC_URL=https://eth-sepolia.g.al
+#CONTRACT_ADDRESS=0x29E
+WALLET_ADDRESS=0xCeC73
+PRIVATE_KEY=0xb5
+
+CONTRACT_NAME=DeployGetGift
