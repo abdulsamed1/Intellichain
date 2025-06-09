121b53135ca4e64a7fb07b1a10df58efa2ae7546
abdulsamed <abdalsamed71@gmail.com>
2025-06-09 12:05:13 +0000

    Update .env: restore ETHERSCAN_API_KEY and correct RPC URLs

---
+++ contract/envexample.sh
@@ -0,0 +1,13 @@
+# This file is used to set environment variables for the contract
+ETHERSCAN_API_KEY=
+#get your API key from https://etherscan.io/apidashboard
+MAINNET_RPC_URL=https://mainnet.infura.io/v3/
+SEPOLIA_RPC_URL=https://eth-sepolia.g.alchemy.com/v2/
+# get your RPC URL from https://www.alchemy.com/
+CONTRACT_ADDRESS=0x29EeD516E36f1b71D2a176C64bA0A287e2EaA3E0
+# get your contract address from https://sepolia.etherscan.io/address/0
+WALLET_ADDRESS=0xCe
+# get your wallet address from your wallet
+PRIVATE_KEY=0xf57
+# get your private key from your wallet
+CONTRACT_NAME=GetGift
