2b843dad71fdeb54c2f74f72bf9daa2b2d431462
abdulsamed <abdalsamed71@gmail.com>
2025-06-09 13:34:53 +0000

    Update envexample.sh: restore and correct environment variable values

--- contract/envexample.sh
+++ contract/envexample.sh
@@ -1,13 +1,19 @@
 # This file is used to set environment variables for the contract
-ETHERSCAN_API_KEY=
-#get your API key from https://etherscan.io/apidashboard
-MAINNET_RPC_URL=https://mainnet.infura.io/v3/
-SEPOLIA_RPC_URL=https://eth-sepolia.g.alchemy.com/v2/
-# get your RPC URL from https://www.alchemy.com/
+ETHERSCAN_API_KEY=T
+# Replace with your actual Etherscan API key
+MAINNET_RPC_URL=https://mainnet.infura.io/v3/d2
+#get your Infura project ID from https://infura.io/
+SEPOLIA_RPC_URL=https://eth-sepolia.g.alchemy.com/v2/0L
+# get your Alchemy project ID from https://www.alchemy.com/
 CONTRACT_ADDRESS=0x29EeD516E36f1b71D2a176C64bA0A287e2EaA3E0
-# get your contract address from https://sepolia.etherscan.io/address/0
-WALLET_ADDRESS=0xCe
-# get your wallet address from your wallet
-PRIVATE_KEY=0xf57
-# get your private key from your wallet
+# Replace with your actual contract address
+WALLET_ADDRESS=0xCeC733
+# Replace with your actual wallet address
+PRIVATE_KEY=0xf5
+# Replace with your actual private key
 CONTRACT_NAME=GetGift
+# Replace with your actual contract name
+SCRIPT_FILE=script/InteractWithGetGift.s.sol:InteractWithGetGift
+# Replace with your actual interaction file name
+FUNCTION=addToAllowList(address,address)
+# Replace with the actual function you want to call
